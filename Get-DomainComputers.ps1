9a5029b1f474faf37c0684d906816f977e261550
zinhart <aromzinhartdegraca@gmail.com>
2022-03-10 14:27:53 -0800

    Initial Commit All Scripts

---
+++ Get-DomainComputers.ps1
@@ -0,0 +1,27 @@
+# THIS SCRIPT MUST BE RUN FROM A DOMAIN JOINED ACCOUNT
+<# Generate LDAP Provider Path That will input into the Directory Searcher
+The LDAP Provider Path has the form of LDAP://HostName[:PortNumber][/DistinguishedName].
+We will also include the Primary Domain Controller(PDC) as this DC will have the most up to date information about user login and authentication.
+#>
+$domainObj =[System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+$PDC = ($domainObj.PdcRoleOwner).Name
+$SearchString = "LDAP://"
+$SearchString += $PDC + "/"
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+$SearchString += $DistinguishedName
+$SearchString
+<# The samAccountType is an attribute that all user, computer, and group objects have. We will filter computers with 0x30000001 (decimal 805306369).
+#>
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+$Searcher.SearchRoot = $objDomain
+$Searcher.filter="samAccountType=805306369"
+$Result = $Searcher.FindAll()
+Foreach($obj in $Result)
+{
+	Foreach($prop in $obj.Properties)
+	{
+		$prop
+	}
+	Write-Host "------------------------"
+}
