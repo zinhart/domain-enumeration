1b51a68caba9a652ecc4d5ba00486e23a79847e2
zinhart <aromzinhartdegraca@gmail.com>
2022-03-10 14:33:31 -0800

    Minor organization

---
+++ domain-groups/Get-DomainGroups.ps1
@@ -0,0 +1,21 @@
+# THIS SCRIPT MUST BE RUN FROM A DOMAIN JOINED ACCOUNT
+<# Generate LDAP Provider Path That will input into the Directory Searcher
+The LDAP Provider Path has the form of LDAP://HostName[:PortNumber][/DistinguishedName].
+We will also include the Primary Domain Controller(PDC) as this DC will have the most up to date information about user login and authentication.
+#>
+$domainObj =[System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+$PDC = ($domainObj.PdcRoleOwner).Name
+$SearchString = "LDAP://"
+$SearchString += $PDC + "/"
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+$SearchString += $DistinguishedName
+$SearchString
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+$Searcher.SearchRoot = $objDomain
+$Searcher.filter="(objectClass=Group)"
+$Result = $Searcher.FindAll()
+Foreach($obj in $Result)
+{
+	$obj.Properties.name
+}
